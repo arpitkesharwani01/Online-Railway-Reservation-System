e5b2a4765757f4925133b5358e74be478b692ea7
Arpit Kesharwani <109498554+arpitkesharwani01@users.noreply.github.com>
2023-09-05 15:38:49 +0530

    Add files via upload

---
+++ Online reservation system/tempCodeRunnerFile.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<time.h>
+#include<string>
+using namespace std;
+
+// void viewInfo();
+// void bookTicket();
+// void cancelTicket();
+// void admin();
+// void password();
+// void newPassenger();
+// void addTrain();
+// void deleteTrain();
+// void awrite();
+// void aRead();
+// void bookTicketWrite();
+// void viewPassengersRead();
+
+
+// class addData{
+//     public:
+//     char si[10];
+//     char trainNumber[10];
+//     char trainName[20];
+//     char start[10];
+//     char destination[10];
+//     char price[10];
+//     char time[10];
+//     int seat;
+//     }add[1000];
+
+// class bookTicket{
+//     char trainNumber[20];
+//     char name[20];
+//     char phone[20];
+//     char date[20];
+//     int seat;
+// }book[1000];
+
+// // Global variable 
+// int k = 0, u = 0;
+// char trainNumber[100], name[100], phone[100];
+
+int main(){
+    // aRead();
